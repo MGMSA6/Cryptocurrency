0749c80b4202eed6793bbf22a255f54234a44ebe
MGMSA <anand004.af@gmail.com>
2022-02-04 13:59:40 +0530

    3. Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -8,7 +8,11 @@ contract DappToken {
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
     mapping(address => uint256) public balanceOf;
+
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -28,4 +32,32 @@ contract DappToken {
 
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+
+        // Allowance
+        allowance[msg.sender][_spender] = _value;
+
+        // Approval event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value; 
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+
 }
